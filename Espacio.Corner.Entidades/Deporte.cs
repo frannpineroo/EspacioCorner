0dc5a4faa727974847cbc765aaf70a4ffb8a51a4
Francisco Piñero <frannpinero@hotmail.com>
2024-11-14 00:44:25 -0300

    eliminación de entidades

--- Espacio.Corner.Entidades/Deporte.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Espacio.Corner.Entidades
-{
-	public class Deporte
-	{
-        public int IdDeporte { get; set; }
-		public string CodDep { get; set; }
-		public string NombreDep { get; set; }
-    }
-}
